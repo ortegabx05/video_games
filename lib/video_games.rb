d45527b4c0cb10f2998835a0433ab35d9d389eb9
jessi ortega <ortegabx05@gmail.com>
2019-09-27 05:45:42 +0000

    bundle isntall

--- lib/video_games.rb
+++ lib/video_games.rb
@@ -1,6 +1,8 @@
 require_relative "video_games/version"
 require_relative "video_games/cli"
 
+require 'pry'
+
 module VideoGames
   class Error < StandardError; end
   # Your code goes here...
--- lib/video_games/cli.rb
+++ lib/video_games/cli.rb
@@ -2,6 +2,7 @@ class VideoGames::CLI
 
  def call
   puts "Welcome to Gamestop!, here are a list of upcoming video games."
+  binding.pry
  end
 
 end
