6990318eb169eeb7483db29f4e1312550c84c828
jessi ortega <ortegabx05@gmail.com>
2019-10-09 14:40:40 +0000

    cli

--- lib/video_games/cli.rb
+++ lib/video_games/cli.rb
@@ -1,8 +1,35 @@
 class VideoGames::CLI
 
  def call
-  puts "Welcome to Gamestop!, here are a list of upcoming video games."
-  binding.pry
+  puts "Welcome to Gamestop!"
+  get_upcoming_games
+  list_games
+  get_user_name
+  release_date
  end
-
+ 
+ def get_upcoming_games
+   @upcoming_games = VideoGames::UpcomingGames.all
+ end
+ 
+ def list_games
+   puts"Here is an updated list of games being released soon."
+   @upcoming_games.each.with_index(1) do |game,index|
+     puts "#{index}. #{game.title}"
+ end
+ 
+ def get_user_game
+   chosen_game = gets.strip.to_i
+   release_date(chosen_game) if valid_input(chosen_game, @upcoming_games)
+ end
+ 
+ def valid_input(input,data)
+   input.to_i <= data.length && input.to_i > 0
+ end
+ 
+ def release_date(chosen_game)
+   game = @upcoming_game [chosen_game - 1]
+   puts "The release date is #{game}"
+ end
+ 
 end
--- lib/video_games/scraping.rb
+++ lib/video_games/scraping.rb
@@ -2,7 +2,7 @@ class VideoGames::Scaper
   
   def scrape_games
     
-    doc = Nokogiri::HTML(open( ))
+    doc = Nokogiri::HTML(open("https://www.gamestop.com/upcoming-video-games"))
     
   end
   
