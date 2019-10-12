ff716c2e3a84351946eaa6ca6f6bfeb1ebbd6d2d
jessi ortega <ortegabx05@gmail.com>
2019-10-12 03:30:32 +0000

    doc.css

--- lib/video_games/scraper.rb
+++ lib/video_games/scraper.rb
@@ -1,6 +1,12 @@
 class VideoGames::Scraper
   def self.scrape_games
     doc = Nokogiri::HTML(open("https://www.gamestop.com/upcoming-video-games"))
-    binding.pry
+   
+   title = doc.css("div#pre-release h5")
+   
+   title.each do |t|
+   puts t.text
+    end
   end
+  
 end
